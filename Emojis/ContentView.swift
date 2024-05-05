75f7fa18f0d08491187983a4d16e0c623b1336e2
Kelvin Anigboro <rukywe@me.com>
2024-05-05 21:47:11 +0100

    Implement dark mode

--- Emojis/ContentView.swift
+++ Emojis/ContentView.swift
@@ -1,24 +1,60 @@
-//
-//  ContentView.swift
-//  Emojis
-//
-//  Created by Kelvin A on 05/05/2024.
-//
-
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+enum Emoji: String, CaseIterable, Identifiable {
+    case poop = "💩"
+    case ogre = "👹"
+    case mask = "😷"
+    case devil = "😈"
+    case rocket = "🚀"
+
+    var id: String { self.rawValue }
+
+    var description: String {
+        switch self {
+        case .poop: return "Just having a bad day!"
+        case .ogre: return "Feeling monstrous"
+        case .mask: return "Stay safe, wear a mask"
+        case .devil: return "A little mischievous"
+        case .rocket: return "To the moon!"
         }
-        .padding()
     }
 }
 
-#Preview {
-    ContentView()
+struct ContentView: View {
+    @State private var selection: Emoji = .rocket
+    @AppStorage("isDarkMode") private var isDarkMode = false
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                Toggle(isOn: $isDarkMode, label: {
+                    Text("Enable Dark Mode")
+                })
+                .padding()
+
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+
+                Text(selection.description)
+                    .font(.title)
+                    .padding()
+
+                Picker("Select Emoji", selection: $selection) {
+                    ForEach(Emoji.allCases) { emoji in
+                        Text(emoji.rawValue).tag(emoji)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Picker!")
+            .padding()
+        }
+        .preferredColorScheme(isDarkMode ? .dark : .light)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
